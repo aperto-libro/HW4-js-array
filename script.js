2d5cc4803810a03cfc5031d9290ad2a92863e1cd
Vitalii Slobodoniuk <slvitalik24@ukr.net>
2021-11-04 15:32:53 +0200

    added task 10

--- script.js
+++ script.js
@@ -50,6 +50,20 @@ console.log(names); // [Vic, Petya, Jon]
 
 console.log('------------------------------------------------- # 10');
 
+let arr1 = [5, 3, 8, 1];
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+    }
+  }
+}
+
+filterRangeInPlace(arr1, 1, 4);
+
+console.log(arr1);
+
 console.log('------------------------------------------------- # 11');
 
 console.log('------------------------------------------------- # 12');
